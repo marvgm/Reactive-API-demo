750cd3508c760c4be12218a73a02e809347f4628
marcos <marvgm@gmail.com>
2022-01-09 19:18:50 -0300

    Add deploy mysql and app for kubernates

--- db.sql
+++ db.sql
@@ -5,6 +5,14 @@ CREATE TABLE `courses` (
   UNIQUE KEY `coursename` (`coursename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `students` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `registered_on` bigint(20) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
 CREATE TABLE `coursework` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `student_id` int(10) unsigned NOT NULL,
@@ -16,14 +24,6 @@ CREATE TABLE `coursework` (
   CONSTRAINT `coursework_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
   CONSTRAINT `coursework_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `students` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `registered_on` bigint(20) NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 insert  into `students`(`id`,`name`,`registered_on`,`status`) values
 (1,'George Gatheca Updated',1234,1),
